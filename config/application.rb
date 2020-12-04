a20099aa2a363b1de16f7aa5b762f9694002c299
simesabatt <t95203631@gmail.com>
2020-12-04 17:11:32 +0000

    日時の設定を追加

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module KintaiB
+module AttendanceApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.time_zone = 'Asia/Tokyo' # この１行をここに追加します。
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
