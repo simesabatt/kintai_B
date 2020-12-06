555b77257074b7a25997d1338676e56f0a21830c
simesabatt <t95203631@gmail.com>
2020-12-06 15:21:28 +0000

    Finish page-layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#top'
+  
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
