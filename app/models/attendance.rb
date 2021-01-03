91fb8ea76ed775bcd9f1b9e6e053b93f04e79941
simesabatt <t95203631@gmail.com>
2021-01-03 13:54:44 +0000

    出退勤単独での登録を蹴る

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -8,24 +8,20 @@ class Attendance < ApplicationRecord
   validate :finished_at_is_invalid_without_a_started_at
   # 退勤時間が存在しない場合、出勤時間は無効
   validate :started_at_is_invalid_without_a_finished_at
-  # 退勤時間を登録する時、出社時間の方が早い場合は無効
-  # validate :started_at_than_finished_at_fast_if_present
   # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
   validate :started_at_than_finished_at_fast_if_invalid
 
   def finished_at_is_invalid_without_a_started_at
-    errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
+    if (Date.current != worked_on ) && started_at.blank? && finished_at.present?
+      errors.add(:started_at, "が必要です")
+    end
   end
   
   def started_at_is_invalid_without_a_finished_at
-     errors.add(:finished_at, "が必要です") if !Date.current && started_at.present? && finished_at.blank?
+    if (Date.current != worked_on ) && started_at.present? && finished_at.blank?
+      errors.add(:finished_at, "が必要です")
+    end
   end
-
-  # def started_at_than_finished_at_fast_if_present
-  #   if started_at.present? && finished_at.nil?
-  #     errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
-  #   end
-  # end
 
   def started_at_than_finished_at_fast_if_invalid
     if started_at.present? && finished_at.present?
